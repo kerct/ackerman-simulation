50c441dee12a33004c290322625158d3dfb20042
kerct <kerchintian@gmail.com>
2023-04-12 16:10:58 +0800

    Add trajectory for path taken

--- src/ackermann/src/visualizer.cpp
+++ src/ackermann/src/visualizer.cpp
@@ -2,7 +2,12 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <visualization_msgs/Marker.h>
 
+bool newPose = false;
+double x, y;
 void cbPose(const geometry_msgs::PoseStamped::ConstPtr &msg) {
+  x = msg->pose.position.x;
+  y = msg->pose.position.y;
+  newPose = true;
 }
 
 int main(int argc, char **argv) {
@@ -10,12 +15,30 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::Rate loop_rate(10);
 
-  // TODO
   ros::Subscriber pose_sub = nh.subscribe("pose", 1, &cbPose);
-  ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+
+  visualization_msgs::Marker line_strip;
+  line_strip.header.frame_id = "world";
+  line_strip.ns = "trajectory";
+  line_strip.id = 0;
+  line_strip.action = visualization_msgs::Marker::ADD;
+  line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+  line_strip.scale.x = 0.03; // line width
+  // blue colour
+  line_strip.color.b = 1.0;
+  line_strip.color.a = 1.0;
 
   while (ros::ok()) {
-    // ROS_INFO("visualizer running");
+    line_strip.header.stamp = ros::Time::now();
+
+    geometry_msgs::Point p;
+    p.x = x;
+    p.y = y;
+    line_strip.points.push_back(p);
+
+    marker_pub.publish(line_strip);
+    newPose = false;
 
     // update topics
     ros::spinOnce();
